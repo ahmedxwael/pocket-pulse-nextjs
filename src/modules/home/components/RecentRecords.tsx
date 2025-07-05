dfda2455af69cd8ce7c1927089d2370b850a6df3
Ahmed Wael <ahmedwael3919@gmail.com>
2025-07-05 13:55:55 +0300

    refactor(RecentRecords): remove unused service and add data length display
    - Commented out the service object that included getIncomesAction and getExpensesAction to simplify the component.
    - Initialized an empty data array for incomes or expenses.
    - Added a display for the count of recent records found based on the data length.

--- src/modules/home/components/RecentRecords.tsx
+++ src/modules/home/components/RecentRecords.tsx
@@ -1,23 +1,25 @@
-import { getExpensesAction, getIncomesAction } from "@/modules/records/actions";
+import { Expense, Income } from "@/modules/records/types";
 
 type RecentRecordsProps = {
   type: "incomes" | "expenses";
 };
 
-const service = {
-  incomes: getIncomesAction,
-  expenses: getExpensesAction,
-};
+// const service = {
+//   incomes: getIncomesAction,
+//   expenses: getExpensesAction,
+// };
 
 export async function RecentRecords({ type }: RecentRecordsProps) {
-  // const { data } = await service[type]({
-  //   take: 5,
-  // });
+  // const { data } = await service[type]();
+  const data: Income[] | Expense[] = [];
 
   return (
     <div className="flex flex-col gap-6">
       <h2 className="text-2xl font-bold mb-4">Recent {type}</h2>
       {/* <RecordsHistoryTable data={data} /> */}
+      <div className="text-muted-foreground">
+        {data?.length || 0} {type} found
+      </div>
     </div>
   );
 }
